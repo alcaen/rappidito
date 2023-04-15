98a82abe81fab947d8e1d9eb4008cfd495aa3ae5
Alcaen <alejocaicedoenriquez@gmail.com>
2023-04-14 19:35:30 -0500

    Tailwind working navigation added

--- App.jsx
+++ App.jsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View className='flex justify-center items-center bg-red-600 w-full'>
-      <Text className='text-xl font-semibold'>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <View className='flex justify-center items-center bg-cyan-600 w-full min-h-screen'>
+        <Text className='text-xl font-semibold text-center text-red-600'>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style='auto' />
+      </View>
+    </NavigationContainer>
   );
 }
 
